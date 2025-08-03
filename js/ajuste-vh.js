e9bbcfe7878d78c9dcfe65c2059881f18a377eab
Ezequiel Grandía <ezequielgrandia@gmail.com>
2025-08-02 23:48:47 -0300

    v7

--- js/ajuste-vh.js
+++ js/ajuste-vh.js
@@ -2,5 +2,8 @@ function setRealVH() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--real-vh", `${vh}px`);
 }
+
 window.addEventListener("resize", setRealVH);
+window.addEventListener("orientationchange", setRealVH); // Opcional, para mejorar en móviles
+
 setRealVH();
