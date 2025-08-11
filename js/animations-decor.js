cee9a5b99af7c1cdf5845c7ca82dc9d303e2cc51
Ezequiel Grandía <ezequielgrandia@gmail.com>
2025-08-11 18:51:06 -0300

    Decorador derecho ok

--- js/animations-decor.js
+++ js/animations-decor.js
@@ -3,6 +3,8 @@ window.addEventListener("DOMContentLoaded", () => {
     const decorSup2 = document.querySelector(".decor-sup-2");
     const decorInf1 = document.querySelector(".decor-inf-1");
     const decorInf2 = document.querySelector(".decor-inf-2");
+    const decorDer1 = document.querySelector(".decor-der-1");
+    const decorDer2 = document.querySelector(".decor-der-2");
 
     const observer = new IntersectionObserver(
         (entries) => {
@@ -17,7 +19,7 @@ window.addEventListener("DOMContentLoaded", () => {
         { threshold: 0.1 }
     );
 
-    [decorSup1, decorSup2, decorInf1, decorInf2].forEach((decor) => {
+    [decorSup1, decorSup2, decorInf1, decorInf2, decorDer1, decorDer2].forEach((decor) => {
         if (decor) {
             // Agregar animate al cargar para que estén visibles inicialmente
             decor.classList.add("animate");
