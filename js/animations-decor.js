b34a7e82723e7df3a392e4c9cd496eda92faaa3a
Ezequiel Grandía <ezequielgrandia@gmail.com>
2025-08-11 22:05:10 -0300

    decorador izq ok

--- js/animations-decor.js
+++ js/animations-decor.js
@@ -5,6 +5,8 @@ window.addEventListener("DOMContentLoaded", () => {
     const decorInf2 = document.querySelector(".decor-inf-2");
     const decorDer1 = document.querySelector(".decor-der-1");
     const decorDer2 = document.querySelector(".decor-der-2");
+    const decorIzq1 = document.querySelector(".decor-izq");
+    const decorIzq2 = document.querySelector(".decor-izq-2");
 
     const observer = new IntersectionObserver(
         (entries) => {
@@ -19,7 +21,7 @@ window.addEventListener("DOMContentLoaded", () => {
         { threshold: 0.1 }
     );
 
-    [decorSup1, decorSup2, decorInf1, decorInf2, decorDer1, decorDer2].forEach((decor) => {
+    [decorSup1, decorSup2, decorInf1, decorInf2, decorDer1, decorDer2, decorIzq1, decorIzq2].forEach((decor) => {
         if (decor) {
             // Agregar animate al cargar para que estén visibles inicialmente
             decor.classList.add("animate");
